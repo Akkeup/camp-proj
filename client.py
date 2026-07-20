622464bd133f06692b233aa15e10f95fb71375cd
Akkeup <hekkouni@gmail.com>
2026-07-21 01:27:42 +0300

    v2.0 -code server-client chat with sockets, start of encode/decode realisation

---
+++ client.py
@@ -0,0 +1,32 @@
+# Alice 
+
+import socket
+
+from user import User
+
+HOST = "127.0.0.1"
+PORT = 8080
+
+gKey = 2
+pKey = 23
+
+Alice = User(gKey, pKey, secretKey=14)
+A = Alice.generate_public_key()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((HOST, PORT))
+    client.sendall(str(A).encode())
+    
+    data = client.recv(1024)
+    B = int(data.decode())
+
+    sA = Alice.generate_secret_key(B)
+    print(f"Alice's secret key: {sA}")
+
+    while True:
+        msg = input("Client: ")
+        if msg == "exit":
+            break
+        client.sendall(msg.encode())
+        data = client.recv(1024)
+        print(f"Server: {data.decode()}")
---
+++ server.py
@@ -0,0 +1,39 @@
+# Bob
+
+import socket
+
+from user import User
+
+HOST = "127.0.0.1"
+PORT = 8080
+
+gKey = 2
+pKey = 23
+
+Bob = User(gKey, pKey, secretKey=10)
+B = Bob.generate_public_key()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((HOST, PORT))
+    server.listen()
+    connection, address = server.accept()
+
+    with connection:
+        print(f"Connected by {address}")
+        connection.sendall(str(B).encode())
+
+        data = connection.recv(1024)
+        A = int(data.decode())
+
+        sB = Bob.generate_secret_key(A)  
+        print(f"Bob's secret key: {sB}")
+
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            print(f"Client: {data.decode()}")
+            reply = input("Server: ")
+            if reply == "exit":
+                break
+            connection.sendall(reply.encode())
